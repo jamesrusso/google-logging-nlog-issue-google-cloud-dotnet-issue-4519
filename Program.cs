2aec5b1e773d572e416730f27a104b1de628fc75
James Russo <James A. Russo jr@halo3.net>
2020-03-05 15:40:52 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestLogging
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetLogger("foo");
+            logger.Info("Program started");
+        }
+             
+    }
+}
